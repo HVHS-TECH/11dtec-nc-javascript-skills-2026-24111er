762fe93bda15b68c0e77b4559ff831b29eef8171
24111er <24111er@hvhs.school.nz>
2026-04-29 15:13:35 +1200

    29/4/26

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,42 @@
-/* Paste your code from task 3, Maths here */
+/****************************
+T05 Javascript and HTML
+***********************/
+console.log("Running t0.js")
+console.log("Hello World!")
+
+// Variables
+const Output = document.getElementById("spaceForJavaScriptOutput");
+
+var userMoney = 2;
+var userAge = 14;
+var userName = "Emme";
+var currentYear = 2026;
+var newMoney;
+
+
+/*
+Main Code
+*/
+
+Output.innerHTML += "<h2>Added by Javascript</h2>";
+Output.innerHTML += "<p>Hi " + userName + "</p>";
+Output.innerHTML += "<p>As of " + currentYear + " you are " + userAge + " years old</p>";
+Output.innerHTML += "<p>You were born in " + (currentYear - userAge) + "</p>";
+Output.innerHTML += "<p>In 10 years you will be " + (userAge + 10) + "</p>";
+Output.innerHTML += "<p>You have " + userMoney + " dollars</p>";
+Output.innerHTML += "<p>You spend half of your money and now you have " + (newMoney = (userMoney / 2)) + " dollar</p>";
+Output.innerHTML += "<p>Then you get $3, now you have " + (newMoney + 3) + " dollars</p>";
+welcome()
+// console.log("Hi " + userName)
+// console.log("As of " + currentYear + " you are " + userAge + " years old")
+// console.log("You were born in", currentYear - userAge)
+// console.log("In 10 years you will be", userAge + 10)
+// console.log("You have " + userMoney + " dollars")
+// console.log("You spend half of your money and now you have", newMoney = (userMoney / 2), "dollar")
+// console.log("Then you get $3, now you have", newMoney + 3, "dollars")
+
+
+/*
+Functions
+*/
+
--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -1,1 +1,47 @@
-/* Paste your code from task 6 here */
+/****************************
+T07 Functions Parameters
+***********************/
+console.log("Running t07.js")
+console.log("Hello World!")
+
+// Variables
+const Output = document.getElementById("spaceForJavaScriptOutput");
+
+var userMoney = 2;
+var userAge = 14;
+var userName = "Emme";
+var currentYear = 2026;
+var newMoney;
+
+
+/*
+Main Code
+*/
+welcome()
+Output.innerHTML += "<h2>Added by Javascript</h2>";
+Output.innerHTML += "<p>Hi " + userName + "</p>";
+Output.innerHTML += "<p>As of " + currentYear + " you are " + userAge + " years old</p>";
+Output.innerHTML += "<p>You were born in " + (currentYear - userAge) + "</p>";
+Output.innerHTML += "<p>In 10 years you will be " + (userAge + 10) + "</p>";
+Output.innerHTML += "<p>You have " + userMoney + " dollars</p>";
+Output.innerHTML += "<p>You spend half of your money and now you have " + (newMoney = (userMoney / 2)) + " dollar</p>";
+Output.innerHTML += "<p>Then you get $3, now you have " + (newMoney + 3) + " dollars</p>";
+displayProduct("Chocolate bar", 4 )
+displayProduct("Chips", 3 )
+displayProduct("Drink", "2.50" )
+
+
+/*
+Functions
+*/
+
+
+function welcome() {
+    Output.innerHTML = "<h1>Welcome to the Shop</h1>";
+    console.log("h1")
+}
+
+function displayProduct(_name, _price) {
+    Output.innerHTML += "<p>" +_name+ ": $" +_price+ "</p>";
+
+}
