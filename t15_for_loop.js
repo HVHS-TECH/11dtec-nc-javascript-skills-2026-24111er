d55f000bbea661ff2af4279002f5fd07e7f3111e
24111er <24111er@hvhs.school.nz>
2026-05-05 23:49:03 +0000

    May 5, 2026, 11:49 PM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -1,1 +1,17 @@
-/* Paste your code from the last task */
+/****************************
+T15 For Loop
+****************************/
+console.log("Running t015_introduction.js")
+console.log("Hello World!")
+
+// Variables
+// This is a single line comment
+
+/*
+Main Code
+*/
+
+
+/*
+Functions
+*/
