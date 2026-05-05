9ad2962b0f7b9f1669319214ccb857e84ba15c95
24111er <24111er@hvhs.school.nz>
2026-05-05 23:54:27 +0000

    May 5, 2026, 11:54 PM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -4,6 +4,8 @@ T15 For Loop
 console.log("Running t015_introduction.js")
 console.log("Hello World!")
 
+const Output = document.getElementById("spaceForJavaScriptOutput");
+var i;
 // Variables
 // This is a single line comment
 
@@ -15,3 +17,13 @@ Main Code
 /*
 Functions
 */
+
+function getFormInputCo () {
+    let countValue = document.getElementById("Countfield").value;
+    let choiceCount = Number(countValue);
+
+    for (i= choiceCount; i>=0; i--) {
+        Output.innerHTML = "<p>" + i + "bottles of milk on the wall";
+
+    }
+}
