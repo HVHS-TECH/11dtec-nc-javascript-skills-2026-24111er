b60cb8847b21cbe215cc11dd21e345bbd212862d
24111er <24111er@hvhs.school.nz>
2026-05-13 02:45:03 +0000

    May 13, 2026, 2:45 AM

--- t22_the_DOM.js
+++ t22_the_DOM.js
@@ -160,7 +160,11 @@ function getFormInput () {
     
 }
 
-function displayProduct(_name, _price) {
-    OUTPUT.innerHTML += "<p>" +_name+ ": $" +_price+ "</p>";
+// function displayProduct(_name, _price) {
+//     OUTPUT.innerHTML += "<p>" +_name+ ": $" +_price+ "</p>";
 
+// }
+
+function buttonClicked() {
+    
 }
